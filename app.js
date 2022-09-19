6be7ed02906f254f71b8f78aa9b1674397d8b49c
NazPro <najmulbinnurul3@gmail.com>
2022-09-19 16:10:44 +0600

    footer added

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// const btn = document.createElement('button');
+// let counter = 0;
+// btn.type = 'button';
+// // btn.value = 'Open Menu';
+// btn.innerText = 'Open Menu';
+// btn.onclick = () => {
+//   let btn2 = document.createElement('input');
+//   btn2.type = 'button';
+//   btn2.value = 'New Text';
+//   document.body.appendChild(btn2);
+//   counter++;
+//   if (counter === 2) btn.disabled = true;
+// }
+// document.body.appendChild(btn);
